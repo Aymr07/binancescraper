8de00ecbeed7f6b9ecb9554bc27ad8e47bc19fd8
Yousuf <yousuf5550@gmail.com>
2021-03-21 00:11:27 +0400

    gets news now

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
 import requests
 import time
 from bs4 import BeautifulSoup
@@ -7,14 +8,34 @@ page = requests.get(
     "https://www.binance.com/en/support/announcement/c-48?navId=48")
 soup = BeautifulSoup(page.content, 'html.parser')
 title = ""
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/822918524619718726/iofuXEasO545MjY8PoSvQbf_TF0C_dqbOy-jl8av7mLDP06TNsgTkkoqNR-LlvQkks9a')
+
+
 
 while True:
-    first_headline = soup.find_all("a", {"class": "css-1ej4hfo"})[0].text
-    time.sleep(10)
+    first_headline = soup.find("a", {"class": "css-1ej4hfo"}).text
+    time.sleep(3)
     if title == first_headline:
         print("Title is the same")
+        embed = DiscordEmbed(
+            title="New Binance Update!",
+            description=first_headline,
+            color='ffff00',
+        )
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.embeds.pop()
+        title = first_headline
+
     else:
-        print(first_headline)
+        embed = DiscordEmbed(
+            title="New Binance Update!",
+            description=first_headline,
+            color='ffff00',
+        )
+        webhook.add_embed(embed)
+        webhook.execute()
+        webhook.embeds.pop()
         title = first_headline
 
 
