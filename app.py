7167f874a7d36e0a35d22a089e31e4f17737a58b
Yousuf <yousuf5550@gmail.com>
2021-03-20 23:39:35 +0400

    gets news now

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-import urllib.request as ureq
+import requests
+import time
+from bs4 import BeautifulSoup
 
-url = "https://www.binance.com/en/support/announcement/c-48?navId=48"
- 
-data = ureq.urlopen(url).read()
+# Make a request
+page = requests.get(
+    "https://www.binance.com/en/support/announcement/c-48?navId=48")
+soup = BeautifulSoup(page.content, 'html.parser')
+title = ""
+
+while True:
+    first_headline = soup.find_all("a", {"class": "css-1ej4hfo"})[0].text
+    time.sleep(10)
+    if title == first_headline:
+        print("Title is the same")
+    else:
+        print(first_headline)
+        title = first_headline
+
+
+
+
+
+
 
