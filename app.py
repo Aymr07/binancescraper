66e773af6ad62f79c64d82c5583e69b4b0fe2973
Yousuf <yousuf5550@gmail.com>
2021-03-21 00:52:31 +0400

    finalized

--- app.py
+++ app.py
@@ -1,46 +1,41 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import requests
 import time
+import json
 from bs4 import BeautifulSoup
+
+with open('data.json', 'r') as outfile:
+    data = json.load(outfile)
 
 # Make a request
 page = requests.get(
     "https://www.binance.com/en/support/announcement/c-48?navId=48")
 soup = BeautifulSoup(page.content, 'html.parser')
 title = ""
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/822918524619718726/iofuXEasO545MjY8PoSvQbf_TF0C_dqbOy-jl8av7mLDP06TNsgTkkoqNR-LlvQkks9a')
-
+webhook = DiscordWebhook(url=data["webhook"])
 
 
 while True:
-    first_headline = soup.find("a", {"class": "css-1ej4hfo"}).text
-    time.sleep(3)
+    first_a = soup.find("a", {"class": "css-1ej4hfo"})
+    first_headline = first_a.text
+    link = "https://binance.com" + str(first_a.get("href"))
+
     if title == first_headline:
         print("Title is the same")
-        embed = DiscordEmbed(
-            title="New Binance Update!",
-            description=first_headline,
-            color='ffff00',
-        )
-        webhook.add_embed(embed)
-        webhook.execute()
-        webhook.embeds.pop()
-        title = first_headline
 
     else:
         embed = DiscordEmbed(
-            title="New Binance Update!",
             description=first_headline,
             color='ffff00',
+        )
+        embed.set_author(
+            name="Binance New Update",
+            url=str(link),
+            icon_url="https://i.imgur.com/cjH2U9J.png"
         )
         webhook.add_embed(embed)
         webhook.execute()
         webhook.embeds.pop()
         title = first_headline
 
-
-
-
-
-
-
+    time.sleep(300)
